54c0a7205188b65634d4a3ad53b3e3c5dcd5c773
Rahman <rahmansaif.413@gmail.com>
2024-11-13 00:48:43 +0600

    work in progress

--- ProgrammingHero/10thMilestone/secondModule/simple-crud-server/index.js
+++ ProgrammingHero/10thMilestone/secondModule/simple-crud-server/index.js
@@ -1,16 +1,82 @@
-const express=require('express');
-const cors=require('cors')
-const app=express()
-const port=process.env.PORT||5000;
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
 
-// middleware
-app.use(cors())
-app.use(express.json())
+// Middleware
+app.use(cors());
+app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send('Simple Crud is running!')
-})
+const uri = "mongodb+srv://rahmansaif413:cNbMY2LeNJt6mU4n@cluster0.m4uphyt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-app.listen(port,()=>{
-    console.log("simple crud is running!");
-})
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+// Connect to MongoDB and define routes
+async function connectToDatabase() {
+  try {
+    // Connect the client to the MongoDB server
+    await client.connect();
+    console.log("Connected to MongoDB!");
+
+    // Define database and collection
+    const database = client.db("userDB");
+    const usersCollection = database.collection("users");
+
+    app.get('/users',async(req,res)=>{
+        const cursor=usersCollection.find();
+        const result=await cursor.toArray();
+        res.send(result)
+    })
+
+    // POST route to add a new user
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      console.log('new user', user);
+      
+      try {
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+      } catch (error) {
+        console.error("Error inserting user:", error);
+        res.status(500).send({ error: "Failed to insert user" });
+      }
+    });
+
+    app.delete('/users/:id',async(req,res)=>{
+        const id=req.params.id;
+        console.log('please delete',id);
+        const query={_id:new ObjectId(id)}
+        const result=await usersCollection.deleteOne(query);
+        res.send(result)
+
+
+    })
+    
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+
+
+// Basic GET route
+app.get('/', (req, res) => {
+  res.send('Simple CRUD is running!');
+});
+
+// Start the server after connecting to the database
+connectToDatabase().then(() => {
+  app.listen(port, () => {
+    console.log(`Simple CRUD server is running on port ${port}`);
+  });
+}).catch((error) => {
+  console.error("Failed to start server:", error);
+});
