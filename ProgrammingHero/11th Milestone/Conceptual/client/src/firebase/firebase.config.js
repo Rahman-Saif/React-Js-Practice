1b948d0e6a974c7b23960d396ca05c8d8008ddd3
Rahman <rahmansaif.413@gmail.com>
2024-12-22 00:44:47 +0600

    11th milestone

---
+++ ProgrammingHero/11th Milestone/Conceptual/client/src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBSKBVfrA2bzfglVHzhthbgUk-AUhTtZPU",
+  authDomain: "solosphere-ddb4c.firebaseapp.com",
+  projectId: "solosphere-ddb4c",
+  storageBucket: "solosphere-ddb4c.firebasestorage.app",
+  messagingSenderId: "2276836013",
+  appId: "1:2276836013:web:45103d9f8dbadc0279bc24"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ ProgrammingHero/11th Milestone/Conceptual/client/src/layouts/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>
+      {/* Navbar */}
+    <h1>Hello</h1>
+      {/* outlet */}
+
+      {/* footer */}
+    </div>
+  )
+}
+
+export default Main
